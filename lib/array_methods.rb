a336257c93f3b185175ac36ec87f86a5d7d010bf
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-26 11:13:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,7 @@ end
 
 def find_max_value(array)
   # Add your solution here
-  let max = 0
+  max = 0
   array.each do |num|
     if num >= max
       max = num
@@ -16,5 +16,5 @@ end
 
 def find_min_value(array)
   # Add your solution here
-  
+
 end
